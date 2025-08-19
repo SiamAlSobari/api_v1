07b7fcfd372a7a3b30b32ad1d4760815590da8f0
YammD123 <siamqueen52@gmail.com>
2025-08-19 21:26:28 +0700

    feat(): add auth middleware

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -1,10 +1,13 @@
 import { MiddlewareHandler } from "hono";
 import { getCookie } from "hono/cookie";
 import { HttpException } from "../common/helpers/http.exception";
+import { verify } from "hono/jwt";
+import { jwtSeccret } from "../common/helpers/jwt.secret";
 
-export const authMiddleware:MiddlewareHandler = async (c, next) => {
+export const authMiddleware: MiddlewareHandler = async (c, next) => {
     const token = await getCookie(c, "token");
-    if (!token){
-        throw new HttpException("Unauthorized", 401);
-    }
-}
+    if (!token) throw new HttpException("Unauthorized", 401);
+    const payload = await verify(token, jwtSeccret);
+    if (!payload) throw new HttpException("Unauthorized", 401);
+    return next();
+};
