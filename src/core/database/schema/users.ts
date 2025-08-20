8d05f4db5c5c947aaab8864dd4184dec7cf4d662
YammD123 <siamqueen52@gmail.com>
2025-08-20 19:57:45 +0700

    feat(schema): add user schema

--- src/core/database/schema/users.ts
+++ src/core/database/schema/users.ts
@@ -1,6 +1,7 @@
 import { relations } from "drizzle-orm";
 import { int, mysqlTable, serial, timestamp, varchar } from "drizzle-orm/mysql-core";
 import { profilesTable } from "./profiles";
+import { postsTable } from "./posts";
 
 export const usersTable = mysqlTable("users", {
     id: varchar("id", { length: 255 }).notNull().unique().primaryKey(),
@@ -10,8 +11,9 @@ export const usersTable = mysqlTable("users", {
     updatedAt: timestamp("updated_at").defaultNow().onUpdateNow().notNull(),
 });
 
-export const usersRelations = relations(usersTable, ({ one }) => ({
+export const usersRelations = relations(usersTable, ({ one,many }) => ({
     profile: one(profilesTable),
+    posts: many(postsTable),
 }));
 
 export type User = typeof usersTable.$inferSelect;
