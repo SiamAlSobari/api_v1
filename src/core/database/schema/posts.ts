35fa71630a73fd511350def11e04c1a7a804d93a
YammD123 <siamqueen52@gmail.com>
2025-08-20 20:28:07 +0700

    feat(schema): add coloum thumbnail url

--- src/core/database/schema/posts.ts
+++ src/core/database/schema/posts.ts
@@ -6,6 +6,7 @@ export const postsTable = mysqlTable("posts", {
     id: varchar("id", { length: 255 }).notNull().unique().primaryKey(),
     title: varchar("title", { length: 255 }).notNull(),
     content: text("content").notNull(),
+    thumbnailUrl: varchar("thumbnail_url",{length: 255}).notNull(),
     user_id: varchar("user_id", { length: 255 })
         .notNull()
         .references(() => usersTable.id),
