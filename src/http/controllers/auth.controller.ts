da25eb141935a9b0313a5363e96d9f56fe618bf7
YammD123 <siamqueen52@gmail.com>
2025-08-24 15:50:25 +0700

    refactor: improve code formatting and consistency in auth and post controllers

--- src/http/controllers/auth.controller.ts
+++ src/http/controllers/auth.controller.ts
@@ -7,40 +7,25 @@ import { HttpResponse } from "../../common/helpers/http.response";
 import { deleteCookie, setCookie } from "hono/cookie";
 import { authMiddleware } from "../../middlewares/auth.middleware";
 
-const authRepository = new AuthRepository()
-const authService = new AuthService(authRepository)
+const authRepository = new AuthRepository();
+const authService = new AuthService(authRepository);
 export const authController = new Hono<{ Variables: Context }>()
-.post(
-    "/signup",
-    zValidator("json", signUpValidation),
-    async (c) => {
-        const { email, password, name} = c.req.valid("json");
-        const user = await authService.signUp(email, password, name)
+    .post("/signup", zValidator("json", signUpValidation), async (c) => {
+        const { email, password, name } = c.req.valid("json");
+        const user = await authService.signUp(email, password, name);
         return HttpResponse(c, "Created", 201, user);
-    }
-)
-.post(
-    "/signin",
-    zValidator("json", signInValidation),
-    async (c) => {
-        const { email, password} = c.req.valid("json");
-        const user = await authService.signIn(email, password)
-        const token = setCookie(c, "token", user, { path: "/",maxAge: 60 * 60 * 24 * 2 });
+    })
+    .post("/signin", zValidator("json", signInValidation), async (c) => {
+        const { email, password } = c.req.valid("json");
+        const user = await authService.signIn(email, password);
+        const token = setCookie(c, "token", user, { path: "/", maxAge: 60 * 60 * 24 * 2 });
         return HttpResponse(c, "Success", 200, { user, token });
-    }
-)
-.get(
-    "/session",
-    authMiddleware,
-    async (c) => {
-        const user = c.get('user')
-        return HttpResponse(c, "Success", 200, {user});
-    }
-)
-.delete(
-    "/signout",
-    async (c) => {
-        const token = deleteCookie(c,"token")
+    })
+    .get("/session", authMiddleware, async (c) => {
+        const user = c.get("user");
+        return HttpResponse(c, "Success", 200, { user });
+    })
+    .delete("/signout", async (c) => {
+        const token = deleteCookie(c, "token");
         return HttpResponse(c, "Berhasil sign out", 200, {});
-    }
-)
+    });
--- src/http/controllers/post.controller.ts
+++ src/http/controllers/post.controller.ts
@@ -6,24 +6,16 @@ import { HttpResponse } from "../../common/helpers/http.response";
 import PostRepository from "../repositories/post.repository";
 import { authMiddleware } from "../../middlewares/auth.middleware";
 
-const postRepository = new PostRepository()
-const postService = new PostService(postRepository)
+const postRepository = new PostRepository();
+const postService = new PostService(postRepository);
 export const postController = new Hono<{ Variables: Context }>()
-.post(
-    "create",
-    zValidator('form',createPostValidation),
-    authMiddleware,
-    async (c) => {
-        const user = c.get('user');
-        const { thumbnail,description,title } = c.req.valid('form');
+    .post("create", zValidator("form", createPostValidation), authMiddleware, async (c) => {
+        const user = c.get("user");
+        const { thumbnail, description, title } = c.req.valid("form");
         const createdPost = await postService.createPost(thumbnail, title, description, user.id);
-        return HttpResponse(c, "Success", 200, createdPost)
-    }
-)
-.get(
-    "terbaru",
-    async (c) => {
+        return HttpResponse(c, "Success", 200, createdPost);
+    })
+    .get("terbaru", async (c) => {
         const posts = await postService.getPostsNew();
-        return HttpResponse(c, "Success", 200, posts)
-    }
-)
+        return HttpResponse(c, "Success", 200, posts);
+    });
