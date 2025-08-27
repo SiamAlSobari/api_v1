75a86c42a4ee56163e18debcb5bd8baf648031c8
YammD123 <siamqueen52@gmail.com>
2025-08-27 20:45:13 +0700

    feat: add and update auth validation

--- src/http/auth/auth.validation.ts
+++ src/http/auth/auth.validation.ts
@@ -8,5 +8,7 @@ export const signInValidation = z.object({
 export const signUpValidation = z.object({
     email: z.string().email(),
     password: z.string(),
-    name: z.string().min(3).max(100),
+    user_name: z.string().min(3).max(100),
+    first_name: z.string().min(3).max(100),
+    last_name: z.string().min(3).max(100),
 });
